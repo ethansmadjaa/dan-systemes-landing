79eb4ca33136e6f7c38cbbefc4280cffd12913c9
Ethan SMADJA <155528040+ethansmadjaa@users.noreply.github.com>
2026-01-05 19:26:14 +0100

    fomrat files

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -1,23 +1,24 @@
-import { ITestimonial } from "@/types";
-import { siteDetails } from "./siteDetails";
+import { ITestimonial } from '@/types';
+
+import { siteDetails } from './siteDetails';
 
 export const testimonials: ITestimonial[] = [
     {
         name: 'TO_FILL',
         role: 'TO_FILL',
         message: 'TO_FILL',
-        avatar: `${process.env.BASE_PATH || ''}/images/testimonial-1.webp`,
+        avatar: `${process.env.BASE_PATH || ''}/images/testimonial-1.webp`
     },
     {
         name: 'TO_FILL',
         role: 'TO_FILL',
         message: 'TO_FILL',
-        avatar: `${process.env.BASE_PATH || ''}/images/testimonial-2.webp`,
+        avatar: `${process.env.BASE_PATH || ''}/images/testimonial-2.webp`
     },
     {
         name: 'TO_FILL',
         role: 'TO_FILL',
         message: 'TO_FILL',
-        avatar: `${process.env.BASE_PATH || ''}/images/testimonial-3.webp`,
-    },
+        avatar: `${process.env.BASE_PATH || ''}/images/testimonial-3.webp`
+    }
 ];
