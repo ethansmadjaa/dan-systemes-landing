401f117be1b1bd855ea38b40971d42e0e5f7471f
Ethan SMADJA <155528040+ethansmadjaa@users.noreply.github.com>
2026-01-07 19:57:16 +0100

    fix: update background styling in ContactForm component
    - Changed the CSS class for the mask property in the ContactForm component to improve the visual appearance of the background, enhancing the overall user interface.

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -112,7 +112,7 @@ const ContactForm: React.FC = () => {
     return (
         <section className='relative flex items-center justify-center px-5 py-16 md:py-24'>
             <div className='absolute top-0 bottom-0 left-0 -z-10 w-full'>
-                <div className='bg-hero-background absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)] bg-[size:40px_40px]'></div>
+                <div className='bg-hero-background absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] mask-[radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)] bg-size-[40px_40px]'></div>
             </div>
 
             <div className='w-full max-w-lg'>
